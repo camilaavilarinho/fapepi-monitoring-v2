b61cb3d7798863362b2af92cf0899ac495e628d1
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-01-12 11:27:46 -0300

    Adding buttons and data biding

--- client/infographic.js
+++ client/infographic.js
@@ -45,77 +45,85 @@ d3.json("data/graphs.json", function(json) {
 });
 
 function mouseover(d) {
- district_data = data[d.properties['dist_code']];
- d3.select("#info").classed("hide", false);
- d3.select("#intro").classed("hide", true);
- d3.select("#infoname").text(d.properties['NM_MICRO']);
- //d3.select("#infoyear").text(district_data[0]['acad_year']);
- d3.select("#infovaluem").text(district_data[0]['msc']);
- d3.select("#infovaluep").text(district_data[0]['phd']);
+  district_data = data[d.properties['dist_code']];
+  d3.select("#info").classed("hide", false);
+  d3.select("#intro").classed("hide", true);
+  d3.select("#infoname").text(d.properties['NM_MICRO']);
+  //d3.select("#infoyear").text(district_data[0]['acad_year']);
+  d3.select("#infovaluem").text(district_data[0]['msc']);
+  d3.select("#infovaluep").text(district_data[0]['phd']);
 }
 
 function mouseout(){
-d3.select("#info").classed("hide", true);
-d3.select("#intro").classed("hide", false);
+  d3.select("#info").classed("hide", true);
+  d3.select("#intro").classed("hide", false);
 }
 
 function clicked(d, district){
-selected_district_data = d;
-if (clicked_flag == false) {
-  selected_district = district;
-  d3.select(district).classed("clicked", true);
-  districts.selectAll("path").on("mouseout", null);
-  districts.selectAll("path").on("mouseover", null);
-  draw(chart_data[selected_district_data.properties['dist_code']]);
-  clicked_flag = true;
-}
-else {
-  old_district = selected_district;
-  selected_district = district;
-  d3.select(old_district).classed("clicked", false);
-  d3.select(district).classed("clicked", true);
-  redraw(chart_data[selected_district_data.properties['dist_code']]);
-  all_districts
-  .on("mouseover", mouseover)
-  .on("mouseout", mouseout);
-};
-mouseover(selected_district_data);
-}
-
-//d3.select("#btn-phd").on("click", draw(data));
-
-
-var n = 6, // number of samples
-    m = 2; // number of series
-
-    var w = 435,
-    h = 200;
-    /*
-    colors = ["#9ECAE1", "#08306B"];*/
-
-    var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d, i) {
-      return areas[i];
-    })
-
-
-    var svg = dimple.newSvg("#graphs", w, h);
-
-    function draw(data) {
-      svg.selectAll('*').remove();
-
-      mest = data.mestrado;
-      var chart = new dimple.chart(svg, mest);
-      chart.addCategoryAxis("x", "name");
-      chart.addMeasureAxis("y", "score");
-      chart.addSeries(null, dimple.plot.bar);
-      chart.draw();
-
-   }
-
-   function redraw(data) {
-     draw(data);
-
+  selected_district_data = d;
+  if (clicked_flag == false) {
+    selected_district = district;
+    d3.select(district).classed("clicked", true);
+    districts.selectAll("path").on("mouseout", null);
+    districts.selectAll("path").on("mouseover", null);
+    draw(chart_data[selected_district_data.properties['dist_code']]);
+    clicked_flag = true;
   }
+  else {
+    old_district = selected_district;
+    selected_district = district;
+    d3.select(old_district).classed("clicked", false);
+    d3.select(district).classed("clicked", true);
+    redraw(chart_data[selected_district_data.properties['dist_code']]);
+    all_districts
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout);
+  };
+  mouseover(selected_district_data);
+}
+
+var w = 435,
+h = 200;
+/*
+colors = ["#9ECAE1", "#08306B"];*/
+
+
+var svg = dimple.newSvg("#graphs", w, h);
+
+function draw(data) {
+  svg.selectAll('*').remove();
+  var data_chart = data.mestrado;
+  var chart = new dimple.chart(svg, data_chart);
+  chart.addCategoryAxis("x", "area");
+  chart.addMeasureAxis("y", "quantidade");
+  chart.addSeries(null, dimple.plot.bar);
+  chart.draw();
+}
+
+function redraw(data) {
+  draw(data);
+}
+
+function drawPhd(data) {
+  svg.selectAll('*').remove();
+  var data_chart = data.doutorado;
+  var chart = new dimple.chart(svg, data_chart);
+  chart.addCategoryAxis("x", "area");
+  chart.addMeasureAxis("y", "quantidade");
+  chart.addSeries(null, dimple.plot.bar);
+  chart.draw();
+}
+
+function change(a){
+  d3.selectAll(".label").classed("label-info", false);
+  d3.selectAll("."+a).classed("label-info", true);
+  if (clicked_flag) {
+    if(a =="msc"){
+      mouseover(selected_district_data);
+      draw(chart_data[selected_district_data.properties['dist_code']]);
+    }else{
+      mouseover(selected_district_data);
+      drawPhd(chart_data[selected_district_data.properties['dist_code']]);
+    }
+  };
+}
